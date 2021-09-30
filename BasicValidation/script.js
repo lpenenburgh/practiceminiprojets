6fba6a9ee60c220f521adf0679b1dd9d0f114b64
lpenen <75550501+lpenenburgh@users.noreply.github.com>
2021-09-30 16:58:44 -0600

    js refactored

--- BasicValidation/script.js
+++ BasicValidation/script.js
@@ -1,3 +1,5 @@
+//refactored version
+
 //variables
 const form = document.getElementById("form");
 const username = document.getElementById("username");
@@ -25,44 +27,66 @@ function showSuccess(input) {
 
 //check if email is valid using javascript email regex
 //https://stackoverflow.com/questions/46155/how-to-validate-an-email-address-in-javascript
-function isValidEmail(email) {
+function checkEmail(input) {
     const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return re.test(String(email).toLowerCase());
+    //return re.test(String(email).toLowerCase());
+    if(re.test(input.value.trim())) {
+        showSuccess(input);
+    } else {
+        showError(input, "Email is not valid");
+    }
+}
+
+// check required fields
+//"input" represents each array item
+function checkRequired(inputArr) {
+    inputArr.forEach(function(input) {
+        //console.log(input.value);
+        if(input.value.trim() === "") {
+            //if we want to capitalize the first letter of the input field name in the message, we make another function we will call getFieldName that will do that for us below
+            showError(input, `${getFieldName(input)} is required`);
+        } else {
+            showSuccess(input);
+        }
+    });
+}
+
+//check input length
+function checkLength(input, min, max) {
+    if (input.value.length < min) {
+        showError(input, `${getFieldName(input)} must be at least ${min} characters`);    
+    } else if(input.value.length > max) {
+        showError(input, `${getFieldName(input)} must be less than ${max} characters`); 
+    } else {
+        showSuccess(input);
+    }
 }
 
 
+//check passwords match
+function checkPasswordsMatch(input1, input2) {
+    if (input1.value !== input2.value) {
+        //to show the error message on the 2nd pass input
+        showError(input2, "Passwords do not match");
+    }
+}
+
+//get fieldname function to uppercase its first letter-we will cut out the first letter, make it uppercase, and then join the rest of the word
+//charAt = character at. the zero is the positioning of the 1st letter
+//The slice() method extracts parts of a string and returns the extracted parts in a new string. 
+function getFieldName(input) {
+    return input.id.charAt(0).toUpperCase() + input.id.slice(1);
+}
 
 //Event listener
 form.addEventListener("submit", function(e) {
     e.preventDefault();
-    // console.log(username.value) will log what the user types in the username field.
-    if (username.value === "") {
-        showError(username, "username is required");
-    } else {
-        showSuccess(username);
-    }
-// need an if/else for every input on the form
-    if (email.value === "") {
-        showError(email, "email is required");
-        //else if the value of the email entered returns not valid from the isValidEmail function from above 
-    } else if(!isValidEmail(email.value)) {
-        showError(email, "email is not valid");        
-    } else {
-        showSuccess(email);
-    }
-
-    if (password.value === "") {
-        showError(password, "password is required");
-    } else {
-        showSuccess(password);
-    }
-
-    if (password2.value === "") {
-        showError(password2, "password is required");
-    } else {
-        showSuccess(password2);
-    }
-
-    
-
+ 
+    //checkRequired(username); if we used this, we would have to have this function over and over for each input. Instead of passing in one input variable at a time, use an array of all the input variables.//
+    checkRequired([username, email, password, password2]);
+    // parameters match the function params of (input, min, max)
+    checkLength(username, 3, 15);
+    checkLength(password, 6, 25);
+    checkEmail(email);
+    checkPasswordsMatch(password, password2);
 });
