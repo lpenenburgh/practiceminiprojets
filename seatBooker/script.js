660fcac855da44ffe48648bc0ed1e857b855d294
lpenen <75550501+lpenenburgh@users.noreply.github.com>
2021-10-04 17:51:37 -0600

    seat selection logic

--- seatBooker/script.js
+++ seatBooker/script.js
@@ -9,4 +9,28 @@ const movieSelect = document.getElementById("movie");
 // to change the movieSelect.value type from a string to a number, use +. (Could also wrap in parseInt)
 const ticketPrice = +movieSelect.value;
 
-console.log(typeof ticketPrice);
+//console.log(typeof ticketPrice);
+
+//update total count in <p>
+function updateSelectedCount() {
+    const selectedSeats = document.querySelectorAll(".row .seat.selected");
+    // get the # of seats selected
+   const selectedSeatsCount = selectedSeats.length;
+
+   count.innerText = selectedSeatsCount;
+   total.innerText = selectedSeatsCount * ticketPrice;
+}
+
+
+
+//event listeners- adding it to the container instead of each seat 
+container.addEventListener("click", (e) => {
+    //conditional to target available seats only
+    if(e.target.classList.contains("seat") && 
+    !e.target.classList.contains("occupied")) {
+        //Toggle between adding and removing selected(blue color) class name 
+        e.target.classList.toggle("selected");
+
+        updateSelectedCount();
+    }
+});
