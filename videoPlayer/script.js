f5501fc32a65d6a55d59627dc65c60668525f6e4
lpenen <75550501+lpenenburgh@users.noreply.github.com>
2021-11-12 12:42:37 -0700

    play&pause functions

--- videoPlayer/script.js
+++ videoPlayer/script.js
@@ -7,8 +7,41 @@ const timeStamp = document.getElementById("timestamp");
 
 
 
+//Play and pause video (when clicking either video itself or play icon)
+//play() and pause() are methods
+function toggleVideoStatus() {
+    if(video.paused) {
+        video.play();
+    } else {
+        video.pause();
+    }
+}
 
+//update the play/pause icon (without this it stays as play icon when paused)
+function updatePlayIcon() {
+    if(video.paused){
+        play.innerHTML = '<i class="fa fa-play fa-2x"></i>';
+    } else {
+        play.innerHTML = '<i class="fa fa-pause fa-2x"></i>';
+    }
+}
 
+//update progress & timestamp
+function updateProgress() {
+    return true;
+}
+
+//set video time to progress
+function setVideoProgress() {
+    return true;
+}
+
+//stop video
+//currentTime is a property. By setting it to zero we are resetting the video to the beginning. And then it pauses (which basically is stopping it)
+function stopVideo() {
+    video.currentTime = 0;
+    video.pause();
+}
 
 //event listeners
 //toggle= play video if its stopped & vis versa
