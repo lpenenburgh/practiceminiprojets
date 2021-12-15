360fecc9a497b83f9de5590a56d7f5b33d5ab7b1
lpenen <75550501+lpenenburgh@users.noreply.github.com>
2021-12-15 12:12:33 -0700

    filter millionaires

--- domArrayMethods/script.js
+++ domArrayMethods/script.js
@@ -50,6 +50,13 @@ function sortByRichest() {
     updateDOM();
 }
 
+// filter only millionaires
+function showMillionaires() {
+    data = data.filter(user => user.money > 1000000);
+
+    updateDOM();
+}
+
 //Add new object to data array
 function addData(obj) {
     data.push(obj);
@@ -82,3 +89,4 @@ function formatMoney(number) {
 addUserBtn.addEventListener('click', getRandomUser);
 doubleBtn.addEventListener('click', doubleMoney);
 sortBtn.addEventListener('click', sortByRichest);
+showMillionairesBtn.addEventListener('click', showMillionaires);
