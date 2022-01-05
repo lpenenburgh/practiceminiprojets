a9ddcfb30670c222c6647977301d2104cf81b681
lpenen <75550501+lpenenburgh@users.noreply.github.com>
2022-01-05 16:02:30 -0700

    js wrong letters restart

--- HangMan/script.js
+++ HangMan/script.js
@@ -1,6 +1,6 @@
 const wordEl = document.getElementById('word');
 const wrongLettersEl = document.getElementById('wrong-letters');
-const playAgainBtn = document.getElementById('play-again');
+const playAgainBtn = document.getElementById('play-button');
 const popup = document.getElementById('popup-container');
 const notification = document.getElementById('notification-container');
 const finalMessage = document.getElementById('final-message');
@@ -45,7 +45,28 @@ function displayWord() {
 
 // update the wrong letters array
 function updateWrongLettersEl() {
-    console.log('update wrong');
+    // display wrong letters
+    wrongLettersEl.innerHTML = `
+        ${wrongLetters.length > 0 ? '<p>Wrong</p>' : ''}
+        ${wrongLetters.map(letter => `<span>${letter}</span>`)}
+    `;
+
+    // display stick figure parts
+    figureParts.forEach((part, index) => {
+        const errors = wrongLetters.length;
+
+        if(index < errors) {
+            part.style.display = 'block';
+        } else {
+            part.style.display = 'none';
+        }
+    });
+
+    // Check if lost 
+    if(wrongLetters.length === figureParts.length) {
+        finalMessage.innerText = 'You Lost';
+        popup.style.display = 'flex';
+    }
 }
 
 
@@ -87,5 +108,20 @@ window.addEventListener('keydown', e => {
         }
     }
 });
+
+// Restart Game and play again
+playAgainBtn.addEventListener('click', () => {
+    // empty arrays
+    correctLetters.splice(0);
+    wrongLetters.splice(0);
+
+    selectedWord = words[Math.floor(Math.random() * words.length)];
+
+    displayWord();
+
+    updateWrongLettersEl();
+
+    popup.style.display = 'none';
+})
 
 displayWord();
